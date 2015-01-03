b1749ddba80d8c54bb7591fe9ccf08e154e3f0fc
John Tan <tantai.john@gmail.com>
2015-01-02 20:15:59 -0800

    Saves processes by PID

--- spinupNodes.js
+++ spinupNodes.js
@@ -9,9 +9,13 @@ NodesManager.prototype.startInit = function() {
   this.config.nodes.forEach(function(node){
     node.args = node.args || [];
     node.args[0] = this.config.rootDir + node.dir + '/' + node.args[0];
-    this.procs[node.name] = spawn('node', node.args)
-    this.procs[node.name].stdout.on('data', function(data){
+    var proc = spawn('node', node.args);
+    this.procs[node.name] = this.procs[node.name] || {};
+    this.procs[node.name][proc.pid] = proc;
+    this.procs[node.name][proc.id].stdout.on('data', function(data){
       console.log('from :', node.name, '\n', data.toString())
     })
   }.bind(this));
 };
+
+NodesManager
