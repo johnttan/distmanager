a5b0f0fe869e4d689bacb0ffe32a8f883623c86e
John Tan <tantai.john@gmail.com>
2015-01-03 14:35:11 -0800

    Added Stop/Start individual nodes buttons

--- public/controls.jsx
+++ public/controls.jsx
@@ -19,6 +19,36 @@ var PostButton = React.createClass({
   }
 });
 
+var PostInput = React.createClass({
+  sendPost: function(){
+    if(this.state.input){
+      var url = this.props.url + this.state.input;
+      $.ajax({
+        url: url,
+        method: 'POST',
+        success: function(data){
+        }.bind(this),
+        error: function(xhr, status, err){
+          console.error(url, status, err.toString())
+        }.bind(this)
+      })
+    }
+  },
+  listenChange: function(e){
+    this.setState({input:e.target.value})
+  },
+  render: function(){
+    return (
+        <div className="ui action input">
+          <input id={this.name}/>
+          <div className="ui button" onClick="sendPost">
+            {this.props.buttonText}
+          </div>
+        </div>
+      )
+  }
+})
+
 React.render(
   <PostButton url="controls/startinit" buttonText="Start Initial Nodes"/>,
   document.getElementById('startInit')
@@ -27,4 +57,14 @@ React.render(
 React.render(
   <PostButton url="controls/stopall" buttonText="Stop All Nodes"/>,
   document.getElementById('stopAll')
+)
+
+React.render(
+  <PostInput url="controls/stop/" name="stopNode" buttonText="Stop Node"/>,
+  document.getElementById('stopNode')
+)
+
+React.render(
+  <PostInput url="controls/start/" name="startNode" buttonText="Start Node"/>,
+  document.getElementById('startNode')
 )
