05643394ebc9172bd20ee854b03a112bebd5843f
John Tan <tantai.john@gmail.com>
2015-01-02 20:02:59 -0800

    Created start method on NodeServer

--- server.js
+++ server.js
@@ -3,5 +3,12 @@ var NodeManager = require('./spinUpNodes');
 
 var NodesServer = function(config){
   this.nodeManager = new NodeManager(config);
+  this.server = new Hapi.Server();
+  this.server.connection({port: config.port});
+};
 
+NodesServer.prototype.start = function() {
+  this.server.start(function(){
+    console.log('Server running at:', this.server.info.uri)
+  }.bind(this))
 };
