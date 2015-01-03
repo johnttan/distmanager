c0c156bc9e86a4297e2c99e52d396feb846ba125
John Tan <tantai.john@gmail.com>
2015-01-02 20:22:32 -0800

    Added helper function

--- spinupNodes.js
+++ spinupNodes.js
@@ -1,5 +1,19 @@
 var spawn = require('child_process').spawn;
 
+function bindNodes(node){
+  node.args = node.args || [];
+  node.args[0] = this.config.rootDir + node.dir + '/' + node.args[0];
+  var proc = spawn('node', node.args);
+  this.procs[node.name] = this.procs[node.name] || {};
+  this.procs[node.name][proc.pid] = proc;
+
+  returnPIDs.push(proc.pid);
+  this.procsIds[proc.pid] = proc;
+  this.procs[node.name][proc.id].stdout.on('data', function(data){
+    console.log('from :', node.name, '\n', data.toString())
+  })
+}
+
 var NodesManager = function(config){
   this.config = config;
   this.procs = {};
@@ -8,21 +22,13 @@ var NodesManager = function(config){
 
 NodesManager.prototype.startInit = function() {
   var returnPIDs = [];
-  this.config.nodes.forEach(function(node){
-    node.args = node.args || [];
-    node.args[0] = this.config.rootDir + node.dir + '/' + node.args[0];
-    var proc = spawn('node', node.args);
-    this.procs[node.name] = this.procs[node.name] || {};
-    this.procs[node.name][proc.pid] = proc;
-
-    returnPIDs.push(proc.pid);
-    this.procsIds[proc.pid] = proc;
-    this.procs[node.name][proc.id].stdout.on('data', function(data){
-      console.log('from :', node.name, '\n', data.toString())
-    })
-  }.bind(this));
+  this.config.nodes.forEach(bindNodes.bind(this));
 
   return returnPIDs;
+};
+
+NodesManager.prototype.start = function(node) {
+
 };
 
 module.exports = NodesManager;
