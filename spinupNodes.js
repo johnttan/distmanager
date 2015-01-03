7a7524c86a48e049809a6878cd88c9f151ddfd2d
John Tan <tantai.john@gmail.com>
2015-01-02 19:55:10 -0800

    Changed to pseudoclassical

--- spinupNodes.js
+++ spinupNodes.js
@@ -1,15 +1,21 @@
 var spawn = require('child_process').spawn;
 var config = require('./spinupConfig');
 
-var procs = {};
 
-config.nodes.forEach(function(node){
-  node.args = node.args || [];
-  node.args[0] = config.rootDir + node.dir + '/' + node.args[0];
-  procs[node.name] = spawn('node', node.args)
-  procs[node.name].stdout.on('data', function(data){
-    console.log('from :', node.name, '\n', data.toString())
-  })
-});
+var SpinupManager = function(config){
+  this.config = config;
+  this.procs = {};
+};
+
+SpinupManager.prototype.startInit = function() {
+  this.config.nodes.forEach(function(node){
+    node.args = node.args || [];
+    node.args[0] = this.config.rootDir + node.dir + '/' + node.args[0];
+    this.procs[node.name] = spawn('node', node.args)
+    this.procs[node.name].stdout.on('data', function(data){
+      console.log('from :', node.name, '\n', data.toString())
+    })
+  }.bind(this));
+};
 
 
