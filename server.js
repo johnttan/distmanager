d93b60ccbd9ca03852876440f70d039bb69749c2
John Tan <tantai.john@gmail.com>
2015-01-02 20:11:06 -0800

    Created routes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var Hapi = require('hapi');
 var NodeManager = require('./spinUpNodes');
+var route = require('./routes');
 
 var NodesServer = function(config){
   this.nodeManager = new NodeManager(config);
   this.server = new Hapi.Server();
+  route(this.server, this.nodeManager);
   this.server.connection({port: config.port});
 };
 
